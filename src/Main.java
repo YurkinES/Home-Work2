ab08d165351d289aa6dc5d2c53e56ef8855d5aee
YurkinES <6eJlomop@inbox.ru>
2023-02-13 00:59:12 +0300

    Eight Stage

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,34 @@
 public class Main {
     public static void main(String[] args) {
+       // Task #1
        var dog = 8.0;
        var cat = 3.6;
        var paper = 763789;
        System.out.println(dog);
        System.out.println(cat);
        System.out.println(paper);
+       // Task #2
        dog = dog + 4.0;
        cat = cat + 4.0;
        paper = paper + 4;
        System.out.println(dog);
        System.out.println(cat);
        System.out.println(paper);
+       //// Task #3
        dog = dog - 3.5;
        cat = cat - 1.6;
        paper = paper - 7639;
        System.out.println(dog);
        System.out.println(cat);
        System.out.println(paper);
+       // Task #4
        var friend = 19;
        System.out.println(friend);
        friend = friend + 2;
        System.out.println(friend);
        friend = friend / 7;
        System.out.println(friend);
+       // Task 5
        var frog = 3.5;
        System.out.println(frog);
        frog = frog * 10;
@@ -32,5 +37,18 @@ public class Main {
        System.out.println(frog);
        frog = frog + 4;
        System.out.println(frog);
+       // Task #6-7
+       var FirstBoxerWeight = 78.2;
+       var SecondBoxerWeight = 82.7;
+       var TotalBoxersWeight = FirstBoxerWeight + SecondBoxerWeight;
+       System.out.println(TotalBoxersWeight);
+       var WeightDifference = SecondBoxerWeight - FirstBoxerWeight;
+       System.out.println(WeightDifference);
+       WeightDifference = SecondBoxerWeight % FirstBoxerWeight;
+       System.out.println(WeightDifference);
+       // Task #8
+
+
+
     }
 }
