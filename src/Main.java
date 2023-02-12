2f821ee2b04d971f54c7dab20b461aac0fe52415
YurkinES <6eJlomop@inbox.ru>
2023-02-13 00:25:05 +0300

    Third Stage

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,11 @@ public class Main {
        System.out.println(dog);
        System.out.println(cat);
        System.out.println(paper);
+       dog = dog - 3.5;
+       cat = cat - 1.6;
+       paper = paper - 7639;
+       System.out.println(dog);
+       System.out.println(cat);
+       System.out.println(paper);
     }
 }
